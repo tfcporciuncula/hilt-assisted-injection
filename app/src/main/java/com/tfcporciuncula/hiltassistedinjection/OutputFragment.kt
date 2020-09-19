3da23e4a37b92a4f6cef8464000a1270db200946
Fred Porciúncula <fred.porciuncula@nufin.de>
2020-09-19 13:39:30 +0200

    Introduce a factory
    Here we’re making things more safe at the cost of verbosity. The more parameters the ViewModel has, the more verbosity you end up with since you have to repeat them all in the factory. Also, injecting the ViewModel is more painful now as we need to first inject the factory, and then ask for the ViewModel.

--- app/src/main/java/com/tfcporciuncula/hiltassistedinjection/OutputFragment.kt
+++ app/src/main/java/com/tfcporciuncula/hiltassistedinjection/OutputFragment.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
 import com.tfcporciuncula.hiltassistedinjection.databinding.FragmentOutputBinding
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class OutputFragment : Fragment(R.layout.fragment_output) {
 
-  private val viewModel by viewModels<OutputViewModel>()
+  private val args by navArgs<OutputFragmentArgs>()
+
+  @Inject lateinit var viewModelFactory: OutputViewModel.Factory
+
+  private val viewModel by viewModels<OutputViewModel> {
+    object : ViewModelProvider.Factory {
+      override fun <T : ViewModel?> create(modelClass: Class<T>) = viewModelFactory.create(args.input) as T
+    }
+  }
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     val binding = FragmentOutputBinding.bind(view)
--- app/src/main/java/com/tfcporciuncula/hiltassistedinjection/OutputViewModel.kt
+++ app/src/main/java/com/tfcporciuncula/hiltassistedinjection/OutputViewModel.kt
@@ -1,23 +1,21 @@
 package com.tfcporciuncula.hiltassistedinjection
 
-import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import javax.inject.Inject
 
 class OutputViewModel @ViewModelInject constructor(
   upperCaseService: UpperCaseService,
-  @Assisted savedStateHandle: SavedStateHandle
+  input: String
 ) : ViewModel() {
 
-  private val outputLiveData = MutableLiveData<String>()
+  class Factory @Inject constructor(private val upperCaseService: UpperCaseService) {
+    fun create(input: String) = OutputViewModel(upperCaseService, input)
+  }
+
+  private val outputLiveData = MutableLiveData(upperCaseService.run(input))
 
   fun outputLiveData(): LiveData<String> = outputLiveData
-
-  init {
-    val input = savedStateHandle.get<String>("input")!!
-    outputLiveData.value = upperCaseService.run(input)
-  }
 }
