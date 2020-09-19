012efee2a6c2e706c189f84adaac24f30406e90d
Fred Porciúncula <fred.porciuncula@nufin.de>
2020-09-19 12:36:26 +0200

    Add extensions to cover other cases
    So far our extension only works if we want a fragment scoped ViewModel. This commit adds more extensions so we cover getting a ViewModel from an activity, an activity scoped ViewModel from a fragment, and finally a nav graph scoped ViewModel from a fragment.

--- app/src/main/java/com/tfcporciuncula/hiltassistedinjection/AssistedViewModel.kt
+++ app/src/main/java/com/tfcporciuncula/hiltassistedinjection/AssistedViewModel.kt
@@ -2,15 +2,48 @@
 
 package com.tfcporciuncula.hiltassistedinjection
 
+import androidx.activity.viewModels
+import androidx.annotation.IdRes
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.AbstractSavedStateViewModelFactory
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.navigation.navGraphViewModels
+
+inline fun <reified T : ViewModel> FragmentActivity.assistedViewModel(
+  crossinline viewModelProducer: (SavedStateHandle) -> T
+) = viewModels<T> {
+  object : AbstractSavedStateViewModelFactory(this, intent.extras) {
+    override fun <T : ViewModel> create(key: String, modelClass: Class<T>, handle: SavedStateHandle) =
+      viewModelProducer(handle) as T
+  }
+}
 
 inline fun <reified T : ViewModel> Fragment.assistedViewModel(
   crossinline viewModelProducer: (SavedStateHandle) -> T
 ) = viewModels<T> {
+  object : AbstractSavedStateViewModelFactory(this, arguments) {
+    override fun <T : ViewModel> create(key: String, modelClass: Class<T>, handle: SavedStateHandle) =
+      viewModelProducer(handle) as T
+  }
+}
+
+inline fun <reified T : ViewModel> Fragment.assistedNavGraphViewModel(
+  @IdRes navGraphId: Int,
+  crossinline viewModelProducer: (SavedStateHandle) -> T
+) = navGraphViewModels<T>(navGraphId) {
+  object : AbstractSavedStateViewModelFactory(this, arguments) {
+    override fun <T : ViewModel> create(key: String, modelClass: Class<T>, handle: SavedStateHandle) =
+      viewModelProducer(handle) as T
+  }
+}
+
+inline fun <reified T : ViewModel> Fragment.assistedActivityViewModel(
+  crossinline viewModelProducer: (SavedStateHandle) -> T
+) = activityViewModels<T> {
   object : AbstractSavedStateViewModelFactory(this, arguments) {
     override fun <T : ViewModel> create(key: String, modelClass: Class<T>, handle: SavedStateHandle) =
       viewModelProducer(handle) as T
